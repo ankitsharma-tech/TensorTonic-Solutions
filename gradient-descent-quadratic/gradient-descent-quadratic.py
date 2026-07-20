0be1b1d4b69b21fdb209a7fed203d1a90e60eb18
Ankit Sharma <132855213+ankitsharma-tech@users.noreply.github.com>
2026-07-20 09:22:39 -0400

    Solve gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -3,16 +3,16 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     """
     Return final x after 'steps' iterations.
     """
-    x = torch.tensor(float(x0),requires_grad=True) 
+    # Write code here
+    x = torch.tensor(float(x0),requires_grad = True)
     
-
     for _ in range(steps):
-        y = a * x**2 + b * x + c
+        y = a* x**2+b*x+c
         y.backward()
-        
         with torch.no_grad():
-            x -= lr * x.grad
-        
+            x-= lr * x.grad
         x.grad.zero_()
+
+        
 
     return x.item()
