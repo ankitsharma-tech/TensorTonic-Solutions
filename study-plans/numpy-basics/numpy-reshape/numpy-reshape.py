50ea2465578a3e12f0be5a8722234481a5f285d5
Ankit Sharma <132855213+ankitsharma-tech@users.noreply.github.com>
2026-09-05 11:20:00 -0400

    Solve numpy-reshape

---
+++ study-plans/numpy-basics/numpy-reshape/numpy-reshape.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def reshape_array(data, operation):
+    data = np.asarray(data,dtype="float64")
+    if operation=="flatten":
+        return data.flatten()
+    elif operation =="transpose":
+        return data.T
+    elif operation == "add_batch":
+        return data[np.newaxis,:]
